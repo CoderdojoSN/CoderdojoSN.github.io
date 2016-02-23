90dd89192319c0d6df6b5935d9bc5f4940cd315a
coderdojosn <sallinsnaas.ie@coderdojo.com>
2016-02-23 14:35:44 +0000

    Update dojo.js

--- js/dojo.js
+++ js/dojo.js
@@ -1,15 +1,21 @@
 
+
 $(document).ready(function(){
+  
 var email = 'mach1982@gmail.com';
 var subject = 'CoderDojo';
-$('mail_butt').click(function() {
-    alert("test");
 
-    var mailto_link = 'mailto:' + email + '?subject=' + subject;
+$('#m').click(function() {
+ 
+ 
+ var mailto_link = 'mailto:' + email + '?subject=' + subject;
 
     win = window.open(mailto_link, 'emailWindow');
     if (win && win.open && !win.closed) win.close();
 
+   
+
 });
 
 });
+
