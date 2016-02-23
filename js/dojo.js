a0281c7b081a61625b6736248ef5aab75232ef2a
coderdojosn <sallinsnaas.ie@coderdojo.com>
2016-02-23 13:20:52 +0000

    Update dojo.js

--- js/dojo.js
+++ js/dojo.js
@@ -1,1 +1,12 @@
 
+
+var email = 'mach1982@gmail.com';
+var subject = 'CoderDojo';
+$('mail_butt').click(function() {
+
+    var mailto_link = 'mailto:' + email + '?subject=' + subject;
+
+    win = window.open(mailto_link, 'emailWindow');
+    if (win && win.open && !win.closed) win.close();
+
+});
